0baab87cf8584468570c23e8e6f90378fa18809e
sikatanju <saadtanju@gmail.com>
2025-04-24 16:51:10 +0600

    implement django signals to notify vendor (printing in the console) when a new order is placed

--- hungry_app/signals.py
+++ hungry_app/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
-from .models import Customer, Vendor
+from .models import Customer, Vendor, Order
 
 
 User = get_user_model()
@@ -14,3 +14,13 @@ def create_customer_or_vendor_for_new_user(sender, instance, created, **kwargs):
             Vendor.objects.create(user=instance)
         elif instance.is_customer:
                 Customer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Order)
+def notify_vendors_on_new_order(sender, instance, created, **kwargs):
+    if not created:
+        return
+    
+    print(f"NEW ORDER NOTIFICATION: Order #{instance.id} is placed")
+    print(f"Customer: {instance.customer.user.username} (ID: {instance.customer.user.id})")
+    print(f"Order Total: ${instance.total_price}")
