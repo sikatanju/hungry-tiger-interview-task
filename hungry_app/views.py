61efd7bebf625c4d00ba3599ae7092dbd126b1e8
sikatanju <saadtanju@gmail.com>
2025-04-24 14:00:50 +0600

    add default pagination of page_size of 10

--- hungry_app/views.py
+++ hungry_app/views.py
@@ -6,10 +6,12 @@ from rest_framework import status
 from .models import Product, Vendor, Customer, OrderItem, Order 
 from .serializers import ProductSerializer, VendorSerializer, CreateOrderSerializer, OrderSerializer, SimpleOrderSerializer
 from .permissions import IsVendorOrReadOnly, IsVendorAndReadOnly, IsCustomerOrReadOnly
+from .pagination import DefaultPagination
 
 class ProductViewSet(ModelViewSet):
     permission_classes = [IsVendorOrReadOnly]
     serializer_class = ProductSerializer
+    pagination_class = DefaultPagination
     
     def get_queryset(self):
         return Product.objects.all()
@@ -31,10 +33,12 @@ class VendorViewSet(ModelViewSet):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
     permission_classes = [IsVendorAndReadOnly]
+    pagination_class = DefaultPagination
 
 
 class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated, IsCustomerOrReadOnly]
+    pagination_class = DefaultPagination
 
     def get_queryset(self):
         if self.request.user.is_staff:
