30f8e3517ad112ba7ce1cb8b539dbf78ca8aa2c3
sikatanju <saadtanju@gmail.com>
2025-04-24 09:39:45 +0600

    import signal to app file

--- hungry_app/apps.py
+++ hungry_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HungryAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hungry_app'
+
+    def ready(self):
+        import hungry_app.signals
